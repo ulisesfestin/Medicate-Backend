cb32ec7fcda3d2c020a06a84e5de68387ad3408f
Ulises <u.festin@alumno.um.edu.ar>
2024-04-18 11:52:49 -0300

    #1 Created SpringBoot Project

---
+++ src/main/java/com/medicateapp/restfulapi/MedicateAppApplication.java
@@ -0,0 +1,13 @@
+package com.medicateapp.restfulapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicateAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicateAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/medicateapp/restfulapi/MedicateAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medicateapp.restfulapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicateAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
